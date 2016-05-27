67f6d0d7709527d149e041483fb7ba135034996a
Mike Voorhees <vernish13@gmail.com>
2016-05-27 19:16:52 -0400

    Idea for goto variation that needs to be implemented

---
+++ RedoxExtensions/Actions.Dispatched/Internal/GotoPointLimited.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedoxExtensions.Actions.Dispatched.Internal
+{
+    /// <summary>
+    /// This action should be used when chaining with the Face action.  This action only works
+    /// if the character is already facing the point we need to go to
+    /// </summary>
+    public class GotoPointLimited
+    {
+    }
+}
